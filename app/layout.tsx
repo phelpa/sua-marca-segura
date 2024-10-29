e2b4cad514c8432e6edcf78ee234ed78e904e2f7
phelpa <pliniohelpa@gmail.com>
2024-10-29 20:30:09 -0300

    try adding facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { keywords } from "./components/keywords";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -61,6 +62,8 @@ export const viewport = {
   initialScale: 1.0,
 };
 
+const FB_PIXEL_ID = "2855624541267230";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -68,6 +71,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="fb-pixel" strategy="afterInteractive">
+        {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${FB_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+      </Script>
       <head>
         <title>Jade Marcas</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
