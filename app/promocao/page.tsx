75c52317dafff7e5070c36e378e2938cb1b3f5c6
phelpa <pliniohelpa@gmail.com>
2025-01-25 22:26:16 -0300

    enhance landing page layout and content for improved user engagement

--- app/promocao/page.tsx
+++ app/promocao/page.tsx
@@ -48,39 +48,91 @@ const jsonLd = {
 
 export default function Home() {
   return (
-    <div className="text-white bg-neutral-900 min-h-screen p-8 sm:p-12 lg:p-16">
+    <div className="text-white bg-black min-h-screen p-16 sm:p-12 lg:p-16 bg-cover bg-center">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-      <div className="flex flex-wrap justify-between items-center gap-8 lg:gap-12">
-        <div className="flex flex-col gap-8 w-full lg:w-2/5 text-center lg:text-left items-center lg:items-start">
-          <div>
-            <Image
-              src="/jademarcas2.svg"
-              alt="Jade Marcas Icon"
-              width={260}
-              height={90}
-              className="mb-2"
-            />
+      <div className="flex flex-col gap-32">
+        <div className="flex flex-wrap justify-between items-center gap-8 lg:gap-12">
+          <div className="flex flex-col gap-8 w-full lg:w-2/5 text-center lg:text-left items-center lg:items-start">
+            <div>
+              <Image
+                src="/jademarcas2.svg"
+                alt="Jade Marcas Icon"
+                width={260}
+                height={90}
+                className="mb-2"
+              />
+            </div>
+            <h2 className="text-4xl sm:text-5xl w-full mt-4">
+              A partir de agora, a sua empresa realmente vai te pertencer
+            </h2>
+            <h2 className="text-2xl sm:text-2xl w-full mt-4 text-white/75">
+              Proteja seu negócio garantindo a exclusividade do nome que você
+              escolheu e construa uma base sólida para o futuro da sua empresa
+            </h2>
           </div>
-          <h2 className="text-4xl sm:text-5xl w-full lg:w-4/5 mt-4">
-            A partir de agora, a sua empresa realmente vai te pertencer
-          </h2>
-          <h2 className="text-2xl sm:text-3xl w-full lg:w-4/5 mt-4 text-white/75">
-            Proteja seu negócio garantindo a exclusividade do nome que você
-            escolheu e construa uma base sólida para o futuro da sua empresa
-          </h2>
         </div>
 
-        <div className="flex justify-center w-full lg:w-2/5">
-          <Image
-            src="/jademarcas.png"
-            alt="jade marcas advogada"
-            width={500}
-            height={500}
-            className="w-[300px] sm:w-[400px] lg:w-[500px] h-auto object-cover"
-          />
+        <div className="flex justify-center items-center flex-col gap-16">
+          <div className="text-center text-4xl lg:w-2/5">
+            Todo negócio tem uma identidade que precisa ser protegida
+          </div>
+          <div className="lg:w-2/5 text-2xl text-center text-white/75">
+            Uma falha na proteção da sua marca pode abrir espaço para imitações,
+            disputas legais e prejuízos
+          </div>
+          <div className="flex gap-12 lg:w-3/5 ">
+            <Image
+              src="/juizbravo.png"
+              width={300}
+              height={300}
+              alt="disputa legal"
+            />
+            <div className="flex flex-col gap-6">
+              <div className="text-3xl">• Sem problema com a justiça</div>
+              <div className="text-2xl text-white/75">
+                A sua marca registrada evita dores de cabeça com a justiça caso
+                alguém queira ter uma marca com o mesmo nome
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-12 lg:w-3/5 items-center">
+            <div className="flex flex-col gap-6">
+              <div className="text-3xl">• Evite prejuízos e retrabalhos</div>
+              <div className="text-2xl text-white/75">
+                Também evita que você tenha ter que refazer todo trabalho de
+                divulgação caso alguém registre a sua marca antes de você
+              </div>
+            </div>
+            <Image
+              src="/mulherfrustrada.jpeg"
+              width={300}
+              height={300}
+              alt="disputa legal"
+            />
+          </div>
+
+          <div className="flex gap-12 lg:w-3/5 items-center">
+            <Image
+              src="/caralogostarbucks.jpeg"
+              width={300}
+              height={300}
+              alt="disputa legal"
+            />
+
+            <div className="flex flex-col gap-6">
+              <div className="text-3xl">
+                • Segurança de ter a sua marca protegida por 10 anos
+              </div>
+              <div className="text-2xl text-white/75">
+                Não tem maior tranquilidade que saber que a sua marca é
+                realmente sua, o nome é seu e você pode crescer sem preocupações
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
