162f122adc0703bac7de51dbd143bcf4b75f9e9d
phelpa <pliniohelpa@gmail.com>
2025-02-11 09:13:53 -0300

    update promotional page styles and content for enhanced clarity and engagement

--- app/promocao/page.tsx
+++ app/promocao/page.tsx
@@ -48,12 +48,12 @@ const jsonLd = {
 
 export default function Home() {
   return (
-    <div className="text-white bg-black min-h-screen p-16 sm:p-12 lg:p-16 bg-cover bg-center">
+    <div className="text-white bg-gray-950 min-h-screen p-16 sm:p-12 lg:p-16 bg-cover bg-center">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-      <div className="flex flex-col gap-32">
+      <div className="flex flex-col ">
         <div className="flex flex-wrap justify-between items-center gap-8 lg:gap-12">
           <div className="flex flex-col gap-8 w-full lg:w-2/5 text-center lg:text-left items-center lg:items-start">
             <div>
@@ -65,7 +65,7 @@ export default function Home() {
                 className="mb-2"
               />
             </div>
-            <h2 className="text-4xl sm:text-5xl w-full mt-4">
+            <h2 className="text-4xl sm:text-5xl w-full mt-4 fw-200 font-light">
               A partir de agora, a sua empresa realmente vai te pertencer
             </h2>
             <h2 className="text-2xl sm:text-2xl w-full mt-4 text-white/75">
@@ -75,23 +75,27 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex justify-center items-center flex-col gap-16">
-          <div className="text-center text-4xl lg:w-2/5">
-            Todo negócio tem uma identidade que precisa ser protegida
+        <div className="flex justify-center items-center flex-col gap-16 mt-16">
+          <div className="text-center text-4xl lg:w-2/5 font-light">
+            Toda empresa tem um patrimônio a proteger e sua marca é um dos bens
+            mais valiosos do seu negócio.
           </div>
           <div className="lg:w-2/5 text-2xl text-center text-white/75">
             Uma falha na proteção da sua marca pode abrir espaço para imitações,
             disputas legais e prejuízos
           </div>
-          <div className="flex gap-12 lg:w-3/5 ">
+          <div className="flex gap-12 lg:w-3/5 p-6 bg-gray-900 border border-gray-500">
             <Image
               src="/juizbravo.png"
-              width={300}
-              height={300}
+              width={200}
+              height={200}
               alt="disputa legal"
             />
-            <div className="flex flex-col gap-6">
-              <div className="text-3xl">• Sem problema com a justiça</div>
+            <div className="flex flex-col gap-6 ">
+              <div className="text-3xl">
+                <span className="text-[#52edb3]">|</span> Sem problema com a
+                justiça
+              </div>
               <div className="text-2xl text-white/75">
                 A sua marca registrada evita dores de cabeça com a justiça caso
                 alguém queira ter uma marca com o mesmo nome
@@ -99,9 +103,12 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex gap-12 lg:w-3/5 items-center">
+          <div className="flex gap-12 lg:w-3/5 items-center p-6 bg-gray-900 border border-gray-500">
             <div className="flex flex-col gap-6">
-              <div className="text-3xl">• Evite prejuízos e retrabalhos</div>
+              <div className="text-3xl">
+                <span className="text-[#52edb3]">|</span> Evite prejuízos e
+                retrabalhos
+              </div>
               <div className="text-2xl text-white/75">
                 Também evita que você tenha ter que refazer todo trabalho de
                 divulgação caso alguém registre a sua marca antes de você
@@ -109,23 +116,24 @@ export default function Home() {
             </div>
             <Image
               src="/mulherfrustrada.jpeg"
-              width={300}
-              height={300}
+              width={200}
+              height={200}
               alt="disputa legal"
             />
           </div>
 
-          <div className="flex gap-12 lg:w-3/5 items-center">
+          <div className="flex gap-12 lg:w-3/5 items-center p-6 bg-gray-900 border border-gray-500">
             <Image
               src="/caralogostarbucks.jpeg"
-              width={300}
-              height={300}
+              width={200}
+              height={200}
               alt="disputa legal"
             />
 
             <div className="flex flex-col gap-6">
               <div className="text-3xl">
-                • Segurança de ter a sua marca protegida por 10 anos
+                <span className="text-[#52edb3]">|</span> Segurança de ter a sua
+                marca protegida por 10 anos
               </div>
               <div className="text-2xl text-white/75">
                 Não tem maior tranquilidade que saber que a sua marca é
@@ -133,6 +141,22 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </div>
+
+        <div className="text-3xl mt-16 mb-16">
+          <span className="text-[#52edb3] mr-3">|</span>IMPORTÂNCIA DO REGISTRO
+          DA MARCA
+        </div>
+        <div className="text-2xl mb-6 font-light">
+          Ter uma marca sem registro é como ter um carro de alto valor sem
+          seguro: parece um bom negócio até que algo inesperado aconteça.
+        </div>
+        <div className="text-2xl font-light">
+          O registro da marca não apenas protege contra riscos, mas também
+          valoriza o seu negócio, aumentando a credibilidade e o
+          profissionalismo. Uma marca protegida se destaca da concorrência e
+          abre novas oportunidades de crescimento. Sem essa proteção, sua marca
+          é um acidente esperando para acontecer.
         </div>
       </div>
     </div>
